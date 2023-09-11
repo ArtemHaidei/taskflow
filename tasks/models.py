52d4c660b1e510044fec4089d9cc5a394cf45a38
Artem <haideiartem@gmail.com>
2023-09-11 23:47:00 +0200

    add tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
+from choiches import TaskPriority
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    if_description = models.BooleanField(default=False)
+    is_done = models.BooleanField(default=False)
+    deadline = models.DateTimeField(blank=True, null=True)
+    priority = models.CharField(max_length=20, choices=TaskPriority.choices, default='Medium')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
